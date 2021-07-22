09291d76424a6ac00b45ee31e86da70522cf6c17
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-07-22 00:23:38 -0400

    Course

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,18 +1,21 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_course")
 public class Course implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,10 +23,14 @@ public class Course implements Serializable {
 	private String imgUri;
 	private String imgGrayUri;
 	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
+	
 	public Course() {
 	}
 
 	public Course(Long id, String name, String imgUri, String imgGrayUri) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.imgUri = imgUri;
@@ -60,6 +67,10 @@ public class Course implements Serializable {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,4 +13,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://as2.ftcdn.net/v2/jpg/03/83/77/33/1000_F_383773350_MixRdQb50qmcrwtLj1fZZ8r0gnwhiAzv.jpg', 'https://as2.ftcdn.net/v2/jpg/00/00/03/57/1000_F_35774_HNOYijt3Iwb7UeoLz01OYD1tQ457ze.jpg');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
