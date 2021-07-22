c01ef37c9163e8be145a962dfa21b1de09e1f36e
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-07-21 21:24:35 -0400

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://as2.ftcdn.net/v2/jpg/03/83/77/33/1000_F_383773350_MixRdQb50qmcrwtLj1fZZ8r0gnwhiAzv.jpg', 'https://as2.ftcdn.net/v2/jpg/00/00/03/57/1000_F_35774_HNOYijt3Iwb7UeoLz01OYD1tQ457ze.jpg');
