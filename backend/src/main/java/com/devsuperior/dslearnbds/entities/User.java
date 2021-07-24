3cd4319e928ddc6185e3965cda3a347f3c58010e
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-07-24 11:02:33 -0400

    Fix: Remove user on constructor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -41,12 +41,11 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+	public User(Long id, String name, String email, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
 	}
 
 	public Long getId() {
