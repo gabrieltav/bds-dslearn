be4fb266074a5b728f783c3b6ec4957d1b7739de
Gabriel Tavares <tavaresgabriel963@gmail.com>
2021-08-01 18:22:56 -0400

    Notification consultation do DSLearn and FIX: database

--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -22,9 +22,9 @@ public class NotificationService {
 	private AuthService authService;
 	
 	@Transactional(readOnly = true)
-	public Page<NotificationDTO> notificationForCurrentUser(Pageable pageable) {
+	public Page<NotificationDTO> notificationForCurrentUser(boolean unreadOnly, Pageable pageable) {
 		User user = authService.authenticated();
-		Page<Notification> page = repository.findByUser(user, pageable);
+		Page<Notification> page = repository.find(user, unreadOnly, pageable);
 		return page.map(x -> new NotificationDTO(x));
 	}
 }
